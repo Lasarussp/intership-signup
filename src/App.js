169d22345a2981174300174f07fef9be2e67e128
Lasarussp <001lasarus@gmail.com>
2022-10-21 11:50:20 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-//import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <div className="registeration">
+        <h1>Register</h1>
+        <labal>Username</labal>
+        <input type="text" />
+        <labal>Password</labal>
+        <input type="password" />
+        <button>Submit</button>
+      </div>
+      <div className="login">
+        <h1>login</h1>
+        <input type="text" placeholder="Username..." />
+        <input type="password" placeholder="password..." />
+        <button>Submit</button>
+      </div>
     </div>
   );
 }
